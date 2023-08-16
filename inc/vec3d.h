ce1e43c9b97a20f7e0f55c5bbae7f94f80a194c4
Aishen <aish3n@pm.me>
2023-08-16 21:47:39 +0800

    style: turn off formatter

--- inc/vec3d.h
+++ inc/vec3d.h
@@ -5,11 +5,11 @@
 
 #define VEC3D_DOT(vec_a, vec_b) (vec_a[0] * vec_b[0] + vec_a[1] * vec_b[1] + vec_a[2] * vec_b[2])
 #define VEC3D_LEN(vec) sqrt(VEC3D_DOT(vec, vec))
-#define VEC3D_MUL(k, vec)                                                                          \
-	{ k * vec[0], k * vec[1], k * vec[2] }
-#define VEC3D_ADD(vec_a, vec_b)                                                                    \
-	{ vec_a[0] + vec_b[0], vec_a[1] + vec_b[1], vec_a[2] + vec_b[2] }
-#define VEC3D_SUB(vec_a, vec_b)                                                                    \
-	{ vec_a[0] - vec_b[0], vec_a[1] - vec_b[1], vec_a[2] - vec_b[2] }
+
+// clang-format off
+#define VEC3D_MUL(k, vec) { k * vec[0], k * vec[1], k * vec[2] }
+#define VEC3D_ADD(vec_a, vec_b) { vec_a[0] + vec_b[0], vec_a[1] + vec_b[1], vec_a[2] + vec_b[2] }
+#define VEC3D_SUB(vec_a, vec_b) { vec_a[0] - vec_b[0], vec_a[1] - vec_b[1], vec_a[2] - vec_b[2] }
+// clang-format on
 
 #endif // VEC3D_H
